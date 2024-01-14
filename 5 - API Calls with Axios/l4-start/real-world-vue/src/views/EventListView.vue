640e801f82d6d35a41ce4deb9ae293ebf6171a29
Harinder Dulai <harinder.dulai@godonate.digital>
2024-01-14 14:06:04 +0000

    install axios and get events from mock db

---
+++ 5 - API Calls with Axios/l4-start/real-world-vue/src/views/EventListView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import axios from 'axios';
+import { ref, onMounted } from 'vue'
+
+const events = ref(null)
+
+onMounted(() => {
+  axios
+  .get('https://my-json-server.typicode.com/haridulai/api/events')
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+})
+</script>
+
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
